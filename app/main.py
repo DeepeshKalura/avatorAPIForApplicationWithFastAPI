795394319d9274703e52fb108587376cb68a6a6b
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-26 19:11:25 +0530

    change the home screen

--- app/main.py
+++ app/main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 templates = Jinja2Templates(directory="template")
 
 @app.get("/", response_class=HTMLResponse)
-async def showing_item(request: Request):
+async def home_screen(request: Request):
     # Render the 'index.html' template and pass any required data
     return templates.TemplateResponse("index.html", {"request": request})
